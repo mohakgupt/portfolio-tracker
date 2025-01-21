59d5e5274dbe857770f69870afc3eb15fd8495de
mohakgupt <thisismohak@gmail.com>
2025-01-21 23:16:34 +0530

    added dockerfile and frontend origin

--- portfolio-tracker-backend/src/main/java/com/mohakgupt/portfoliotrackerbackend/config/CorsConfig.java
+++ portfolio-tracker-backend/src/main/java/com/mohakgupt/portfoliotrackerbackend/config/CorsConfig.java
@@ -2,6 +2,7 @@ package com.mohakgupt.portfoliotrackerbackend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,9 +12,9 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // Add your frontend's origin
+                        .allowedOrigins("https://stellular-macaron-655570.netlify.app/") // Add your frontend's origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
